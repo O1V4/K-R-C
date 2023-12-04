5552f3e7ac3d1cfc6c67542a86955113d3279489
O1V4 <72695556+O1V4@users.noreply.github.com>
2023-12-04 23:46:29 +0200

    exer 1-5

--- chapter-1/1-5.c
+++ chapter-1/1-5.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-void main(void){
+int main(void){
     int cel;
 
     printf("Celsius\t Fahrenheit\n");
     for (cel = 300; cel >= 0; cel = cel -20){
         printf("%3d\t%6.1f\n", cel, (9.0/5.0)*cel+32);
     }
+
+    return 0;
 }
